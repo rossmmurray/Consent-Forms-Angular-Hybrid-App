80483eb66af235a74617a73827a9fa50ceeee3d8
Ross Murray <rossmichaelm@gmail.com>
2019-03-26 22:19:21 +0000

    smiley block now changes

--- src/components/smiley-block/smiley-block.ts
+++ src/components/smiley-block/smiley-block.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {el} from "@angular/platform-browser/testing/src/browser_util";
 
 /**
  * Generated class for the SmileyBlockComponent component.
@@ -12,8 +13,25 @@ import { Component } from '@angular/core';
 })
 export class SmileyBlockComponent {
 
+  public icon1 = '☺︎';
+  public icon2 = '✔︎';
+  public text1 = 'Press this button if you want to take part in the trial ➜';
+  public text2 = "Thanks for your consent!";
+  public buttonIcon: string = this.icon1;
+  public smileyText: string = this.text1;
+
   constructor() {
     console.log('Hello SmileyBlockComponent Component');
+  }
+
+  toggleSmiley() {
+    if (this.buttonIcon === this.icon1) {
+      this.buttonIcon = this.icon2;
+      this.smileyText = this.text2
+    } else {
+      this.buttonIcon = this.icon1;
+      this.smileyText = this.text1;
+    }
   }
 
 }
