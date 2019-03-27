7ea444f39e3cca8ba5606bd5a18712a966fe434f
Ross Murray <rossmichaelm@gmail.com>
2019-03-27 19:54:59 +0000

    can save an image of the screen at least

--- src/pages/documents/documents.ts
+++ src/pages/documents/documents.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {StudyDataProvider} from "../../providers/study-data/study-data";
 import {DomSanitizer} from "@angular/platform-browser";
 import { ToastController } from 'ionic-angular';
+import * as html2canvas from "html2canvas";
 
 
 @IonicPage()
@@ -11,6 +12,9 @@ import { ToastController } from 'ionic-angular';
   templateUrl: 'documents.html',
 })
 export class DocumentsPage {
+  imgDataURL;
+
+  @ViewChild('docPage') docPage: ElementRef;
 
   constructor(
     public navCtrl: NavController,
@@ -25,5 +29,17 @@ export class DocumentsPage {
     console.log('ionViewDidLoad DocumentsPage');
     this.studyDataService.getOneTestFormHTML();
   }
+
+  getDocImage() {
+    let doc = this.docPage.nativeElement;
+    html2canvas(doc).then(canvas => {
+      console.log("doing something at least");
+      console.log(canvas);
+      this.imgDataURL = canvas.toDataURL("image/PNG");
+      // imgDataURL.
+      //Add image Canvas to PDF
+    })
+  }
+
 
 }
